d25dc604f98901c5b3bd5fe28dd0ece8ff246e49
jonatso <jonatan.solheim@ntnu.no>
2022-11-14 11:06:22 +0100

    change image source

--- src/AlgImage.tsx
+++ src/AlgImage.tsx
@@ -3,7 +3,7 @@ import { Image } from "@chakra-ui/react";
 export default function AlgImage({ alg }: { alg: string }) {
     return (
         <Image
-            src={`https://cubing.net/api/visualcube/?fmt=svg&view=plan&sch=fff100,ff4b00,005aff,w,f6aa00,03af7a&case=${alg}`}
+            src={`http://cubiclealgdbimagegen.azurewebsites.net/generator?&puzzle=3&case=${alg}&view=plan&stage=zbll`}
             alt={alg}
         />
     );
