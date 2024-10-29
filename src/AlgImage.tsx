ba770a7ba74cb6bd9b8ed06c3a3356fe54173cfb
jonatso <jonatan.solheim@ntnu.no>
2024-10-29 19:41:54 +0100

    changed image to cubing/twisty

--- src/AlgImage.tsx
+++ src/AlgImage.tsx
@@ -1,18 +1,34 @@
-import { Image } from "@chakra-ui/react";
+import { useEffect, useRef } from "react";
+import { TwistyPlayer } from "cubing/twisty";
+import { Box } from "@chakra-ui/react";
 
-export default function AlgImage({
-    alg,
-    isZBLS,
-}: {
-    alg: string;
-    isZBLS?: boolean;
-}) {
+const AlgImage = ({ alg, isZBLS }: { alg: string; isZBLS?: boolean }) => {
+    const playerRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        const player = new TwistyPlayer({
+            puzzle: "3x3x3",
+            alg,
+            experimentalSetupAnchor: "end",
+            background: "none",
+            experimentalSetupAlg: "x2",
+            controlPanel: "none",
+            visualization: isZBLS ? "experimental-2D-LL" : "experimental-2D-LL",
+        });
+        playerRef.current?.appendChild(player);
+        return () => {
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            playerRef.current?.removeChild(player);
+        };
+    });
     return (
-        <Image
-            src={`https://cube.rider.biz/visualcube.php?fmt=svg&puzzle=3&case=${alg}&view=plan&stage=${
-                isZBLS ? "vh" : "zbll"
-            }&size=200`}
-            alt={alg}
+        <Box
+            ref={playerRef}
+            overflow="hidden"
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
         />
     );
-}
+};
+
+export default AlgImage;
